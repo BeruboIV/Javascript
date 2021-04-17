975635f72ab063f31ef842fd4e60f02ee663f272
BeruboIV <hardikraj13@gmail.com>
2021-04-17 19:41:06 +0530

    Added the backend, frontend is left

---
+++ Portfolio/src/app.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+app.set("view engine", "hbs");
+
+app.get("/", function (req, res) {
+  res.render("index", {
+    title: "Home",
+    name: "Hardik",
+  });
+});
+
+// app.get("/about", function (req, res) {
+//   res.send("<h1>Doom</h1>");
+// });
+
+app.listen("3000", () => {
+  console.log("The port is running at port 3000");
+  console.log(publicDirectoryPath);
+});
