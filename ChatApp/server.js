de29373484f9b0b5dbf2a7026e6dcd4253a9471a
BeruboIV <hardikraj13@gmail.com>
2021-05-10 01:58:15 +0530

    Store the active users

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -5,6 +5,8 @@ const path = require("path");
 const io = require("socket.io")(http);
 const PORT = process.env.PORT || 5000;
 
+const users = {};
+
 // Middlewares
 app.use(express.static(path.join(__dirname)));
 
@@ -14,10 +16,13 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
     console.log(`A user has connected : ${socket.id}`);
-    socket.on("chat-message", (msg) => {
-        // console.log(msg);
-        // io.emit("chat-message", msg);
-        socket.broadcast.emit("chat-message", msg);
+    socket.on("new-user", (name) => {
+        users[socket.id] = name;
+        socket.broadcast.emit("user-joined", name);
+    });
+
+    socket.on("chat-message", (info) => {
+        socket.broadcast.emit("send-message", info);
     });
 
     socket.on("disconnect", () => {
